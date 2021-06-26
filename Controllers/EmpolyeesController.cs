f84a338beb157c92ab96e66037dd21cb99aae9b8
hessenaladawy <hessenaladawy@gmail.com>
2021-06-27 00:06:38 +0200

    add git ingore

--- Controllers/EmpolyeesController.cs
+++ Controllers/EmpolyeesController.cs
@@ -1,6 +1,8 @@
 ﻿using EmployeeManagement.Api.Repository;
 using EmployeeManagement.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,17 +36,59 @@ namespace EmployeeManagement.Api.Controllers
             }
             return result;
         }
+        //public async Task<ActionResult<Employee>> GetEmpoloyeeByEmail(String email)
+        //{
+        //    var result = await repository.Find(x=>x.Email==email);
+            
+        //    if (result == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(result.FirstOrDefault());
+        //}
         [HttpPost]
         public async Task<ActionResult<Employee>> PostEmployee(Employee employee)
         {
            
             if (employee==null )
             {
+                return BadRequest();
+
+            }
+            var emp = await repository.Find(x => x.Email == employee.Email);
+            if (emp.Count() > 0)
+            {
+                ModelState.AddModelError("Email", "Employee email is already exist");
+                return BadRequest(ModelState);
+            }
+            else
+            {
                 var result = await repository.Insert(employee);
+
                 return result;
             }
+              
+            
           
-            return NotFound();
+           
+        }
+        [HttpPut]
+        public async Task<ActionResult< Employee>> UpdateEmployee(Employee employee)
+        {
+            if (employee == null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,"Error Updating data");
+            }
+
+            var result =await repository.Update(employee);
+            await repository.Save();
+            
+
+
+
+
+            return result;
+            
         }
     }
 }
